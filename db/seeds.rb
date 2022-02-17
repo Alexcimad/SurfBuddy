84cd9150c43d292d784dfcd1bc7666ea3f836f7e
theo galais <theogalais@gmail.com>
2022-02-17 20:08:50 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "---DESTROYING CURRENT DATA"
+SpotReview.destroy_all
+SurfSpot.destroy_all
+User.destroy_all
+
+CONST_LEVEL = ["Beginner", "Intermediate", "Expert"]
+surf_spots = []
+
+# Add Users
+
+puts "----CREATING ALEXIS USER"
+user = User.create!(email: 'alex.cimadev@gmail.com', password: "Wagon2021", nickname: "Alexis", level: CONST_LEVEL.sample)
+
+puts "----CREATING THEO USER"
+user1 = User.create!(email: 'theogalais@gmail.com',password: "theogalais@gmail.com", nickname: "Théo", level: CONST_LEVEL.sample)
+
+puts "----CREATING DAVID USER"
+user2 = User.create!(email: 'davidsantos@live.fr',password: "davidsantos@live.fr", nickname: "David", level: CONST_LEVEL.sample)
+
+puts "----CREATING ARON USER"
+user3 = User.create!(email: 'aron@gmail.com',password: "aron@gmail.com", nickname: "Aron", level: CONST_LEVEL.sample)
+
+puts "----CREATING KELLY S. USER"
+kellyS = User.create!(email: 'kellyS@gmail.com',password: "WSL2021", nickname: "Kelly", level: CONST_LEVEL.sample)
+
+puts "----CREATING JEREMY F. USER"
+jeremyF = User.create!(email: 'jeremyF@gmail.com',password: "WSL2021", nickname: "Jeremy", level: CONST_LEVEL.sample)
+
+# Add Spots
+
+surf_spot = SurfSpot.create(location: "Paris", description: "Lorem Ipsum")
+puts "- #{surf_spot.location}"
+
+surf_spot = SurfSpot.create(location: "Quiberon", description: "Lorem Ipsum")
+puts "- #{surf_spot.location}"
+
+surf_spot = SurfSpot.create(location: "La torche", description: "Lorem Ipsum")
+puts "- #{surf_spot.location}"
+
+surf_spot = SurfSpot.create(location: "Biarritz", description: "Lorem Ipsum")
+puts "- #{surf_spot.location}"
+
+
+surf_spots << surf_spot
