9f10ecb9d768e37db912c566fcd5131eb62bce28
David SANTOS <milleunium300@hotmail.com>
2022-02-17 19:46:47 +0100

    page profile

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,6 @@ ActiveRecord::Schema.define(version: 2022_02_17_181219) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "review_spots", force: :cascade do |t|
-    t.string "comment"
-    t.integer "rating"
-    t.bigint "surf_spot_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["surf_spot_id"], name: "index_review_spots_on_surf_spot_id"
-    t.index ["user_id"], name: "index_review_spots_on_user_id"
-  end
-
   create_table "spot_reviews", force: :cascade do |t|
     t.text "comment"
     t.integer "rating"
@@ -62,5 +51,4 @@ ActiveRecord::Schema.define(version: 2022_02_17_181219) do
 
   add_foreign_key "spot_reviews", "surf_spots"
   add_foreign_key "spot_reviews", "users"
-
 end
