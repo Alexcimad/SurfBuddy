55eed34b385ae29d26f917b9751dab376b7635b0
theo galais <theogalais@gmail.com>
2022-02-19 12:25:46 +0100

    Custom show surf spot and add reviews

--- app/controllers/spot_reviews_controller.rb
+++ app/controllers/spot_reviews_controller.rb
@@ -1,2 +1,15 @@
 class SpotReviewsController < ApplicationController
+  def index
+    @spot_reviews = SpotReview.all
+  end
+
+  def show
+    @spot_review = SpotReview.find(params[:id])
+  end
+
+  private
+
+  def spot_reviews_params
+    params.require(:spot_review).permit(:comment, :rating, :user_id, :surf_spot_id)
+  end
 end
