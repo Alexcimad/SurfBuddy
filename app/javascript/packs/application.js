b1ae469b17bf7303224ce9171f7849420960bf6c
theo galais <theogalais@gmail.com>
2022-02-19 15:32:44 +0100

    Add card

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,3 +34,9 @@ document.addEventListener('turbolinks:load', () => {
 });
 
 import "controllers"
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+})
