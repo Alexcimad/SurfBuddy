8ddea638da12c05f9be1ab877640aab4945ca7ed
David SANTOS <milleunium300@hotmail.com>
2022-02-19 11:59:20 +0100

    ok, mais manque méthode geocode

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :profile ]
     def profile
         if params[:level].present?
           @level = params[:level]
