0c670ef873af10db90330b8b83073dcde840fad4
David SANTOS <milleunium300@hotmail.com>
2022-02-19 13:11:57 +0100

    intégration icone favoris partial

--- app/controllers/favorite_spots_controller.rb
+++ app/controllers/favorite_spots_controller.rb
@@ -4,15 +4,35 @@ class FavoriteSpotsController < ApplicationController
 #     @favorite_spots = FavoriteSpot.new
 #   end
 
-  def create
-    @favorite_spots = FavoriteSpot.new(favorite_spots_params)
-    @favorite_spots.user = current_user
-    if @favorite_spots.save
-      redirect_to favorite_spots_path(@favorite_spots)
-    else
-      render :new
+    def favorite
+        raise
+        @user = current_user
+        @surf_spot = SurfSpot.find(params[:id])
+        @favorite = FavoriteSpot.where(user: @user, surf_spot: @surf_spot)
+        if @favorite
+            @favorite.destroy
+        else
+            FavoriteSpot.create(favorite_spots_params)
+        end
     end
-  end
+
+    def create
+        @favorite_spots = FavoriteSpot.new(favorite_spots_params)
+        @favorite_spots.user = current_user
+        if @favorite_spots.save
+        redirect_to favorite_spots_path(@favorite_spots)
+        else
+        render :new
+        end
+    end
+
+    def destroy
+        @favorite_spots = FavoriteSpot.find(params[:id])
+        @favorite_spots.destroy
+
+        # redirect_to tasks_path
+    end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :surf_spots
   get "profile", to: "dashboard#profile"
+  get "favorite", to: "favorite_spot#favorite"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
