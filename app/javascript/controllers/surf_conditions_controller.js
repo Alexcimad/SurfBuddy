83aacf2efc021607488c3a5780c6cd227ca06918
Alexis Cimadevilla <alex.cimadev@gmail.com>
2022-02-21 23:22:56 +0100

    use of API to update cards

--- app/javascript/controllers/surf_conditions_controller.js
+++ app/javascript/controllers/surf_conditions_controller.js
@@ -4,10 +4,11 @@ export default class extends Controller {
   static targets = [ "meteo" ]
 
   connect() {
-    console.log(this.meteoTargets)
+    //console.log(this.meteoTargets[0].lastElementChild.innerText)
   }
 
   meteoTargetConnected(meteo){
+    const apiResponse = {}
     console.log("Target connected")
     console.log(meteo.dataset)
     fetch('https://api.windy.com/api/point-forecast/v2', {
@@ -16,13 +17,14 @@ export default class extends Controller {
       'Accept': 'application/json, text/plain, */*',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({"lat": 43.484750,
-    "lon": -1.558943,
+    body: JSON.stringify({"lat": meteo.dataset.latitude,
+    "lon": meteo.dataset.longitude,
     "model": "gfsWave",
     "parameters": ["waves", "windWaves", "swell1", "swell2"],
-    "key": "ush812LCNxuBzbqH9d7yuakyPxMaoN36"
-  })
+    "key": "ush812LCNxuBzbqH9d7yuakyPxMaoN36"})
     }).then(res => res.json())
-    .then(res => console.log(res));
-    }
+    .then((data) => data['waves_height-surface'])
+    .then((waveHeight) => {meteo.lastElementChild.innerText = waveHeight[0]})
+
+  }
 }
