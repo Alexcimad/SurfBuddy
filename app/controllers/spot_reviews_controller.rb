11b7832100ab15f536e3c4927a69dfe3d788f253
theo galais <theogalais@gmail.com>
2022-02-22 21:33:29 +0100

    controller reviews

--- app/controllers/spot_reviews_controller.rb
+++ app/controllers/spot_reviews_controller.rb
@@ -7,9 +7,26 @@ class SpotReviewsController < ApplicationController
     @spot_review = SpotReview.find(params[:id])
   end
 
+  def new
+    @surf_spot = SurfSpot.find(params[:surf_spot_id])
+    @spot_review = SpotReview.new
+  end
+
+  def create
+    @surf_spot = SurfSpot.find(params[:surf_spot_id])
+    @spot_review = SpotReview.new(spot_review_params)
+    @spot_review.user = current_user
+    @spot_review.surf_spot = @surf_spot
+    if @spot_review.save
+      redirect_to surf_spot_path(@spot_review.surf_spot)
+    else
+      render :new
+    end
+  end
+
   private
 
-  def spot_reviews_params
+  def spot_review_params
     params.require(:spot_review).permit(:comment, :rating, :user_id, :surf_spot_id)
   end
 end
