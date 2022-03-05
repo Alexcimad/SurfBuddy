d51a12bdcf84d164ca8cb882e09bfc398ad62360
David SANTOS <milleunium300@hotmail.com>
2022-03-05 10:50:38 +0100

    simplification design

--- app/javascript/controllers/favorite_controller.js
+++ app/javascript/controllers/favorite_controller.js
@@ -11,10 +11,10 @@ export default class extends Controller {
   }
 
   toggleTag(event) {
-    event.currentTarget.classList.toggle('tag-no-favorite');
-    console.log(this.modalTarget)
-    $('#tagModal').on('hidden.bs.modal', function () {
-      window.alert('hidden event fired!');
-    });
+    event.currentTarget.classList.toggle('card-tags-favorite');
+    // console.log(this.modalTarget)
+    // $('#tagModal').on('hidden.bs.modal', function () {
+    //   window.alert('hidden event fired!');
+    // });
   }
 }
