5ab8c7b24cd23e63b63101ae63509dae82a76766
Alexis Cimadevilla <alex.cimadev@gmail.com>
2022-03-05 15:15:37 +0100

    change default style of map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,7 +5,7 @@ const buildMap = (mapElement) => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     return new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/satellite-v9'
+      style: 'mapbox://styles/mapbox/outdoors-v11'
     });
   };
 
