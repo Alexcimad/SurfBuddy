cb93c2c4a7914fa5185e1d07c5b658fedb01a68c
David SANTOS <milleunium300@hotmail.com>
2022-03-10 20:15:16 +0100

    add reviews ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,11 +28,6 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
-
 import "controllers"
 
 import { initMapbox } from '../plugins/init_mapbox';
@@ -47,5 +42,5 @@ document.addEventListener('turbolinks:load', () => {
   initStarRating();
 })
 
-initStarRating();
+// initStarRating();
 
--- app/javascript/plugins/init_slider.js
+++ app/javascript/plugins/init_slider.js
@@ -3,27 +3,29 @@ import 'owl.carousel';
 import $ from 'jquery';
 
 const initSlider = () => {
-    console.log("Owl carousel init")
-    $('.owl-carousel').owlCarousel({
-        loop: false,
-        margin: 50,
-        items: 1.4,
-        dots:false,
-        nav:false,
-        autoHeight: true,
-        center: true
-    })
-    
-    $('#owl-carousel-hp').owlCarousel({
-        loop: true,
-        margin: 30,
-        items: 1.7,
-        dots:true,
-        nav:true,
-        autoHeight: true,
-        center: true
-    })
-    window.addEventListener ("dragmove", function (event) { event.preventDefault (); }, {passive: false});
+    if (document.querySelector('.owl-carousel')){
+        console.log("Owl carousel init")
+        $('.owl-carousel').owlCarousel({
+            loop: false,
+            margin: 50,
+            items: 1.4,
+            dots:false,
+            nav:false,
+            autoHeight: true,
+            center: true
+        })
+        
+        $('#owl-carousel-hp').owlCarousel({
+            loop: true,
+            margin: 30,
+            items: 1.7,
+            dots:true,
+            nav:true,
+            autoHeight: true,
+            center: true
+        })
+        window.addEventListener ("dragmove", function (event) { event.preventDefault (); }, {passive: false});
+    }
 };
 
 export {initSlider};
