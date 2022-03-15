6bd960f15784bc9299dcd904e3af1807e5b6ab77
Alexis Cimadevilla <alex.cimadev@gmail.com>
2022-03-15 20:09:12 +0100

    add spots in basque country and modify zoom in map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -30,7 +30,7 @@ const buildMap = (mapElement) => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 100, maxZoom: 5 });
+    map.fitBounds(bounds, { padding: 100, maxZoom: 12 });
   };
 
   const initMapbox = () => {
